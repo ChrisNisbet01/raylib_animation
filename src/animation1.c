d35bfd8785d4ae8064dc06c64eaf413dab07e30d
Chris Nisbet <nischris@gmail.com>
2025-11-28 09:07:56 +1300

    Made changes to the animation. It now expands, waits, rotates one way, sleeps, rotates the other way, and then shrinks.

--- src/animation1.c
+++ src/animation1.c
@@ -17,10 +17,9 @@ struct AnimationContext
     coroutine_t * co;
 
     Environment const * env;
-    float elapsed;
-    float resize_time;
     float max_size;
     float current_size;
+    float current_angle;
 };
 
 void
@@ -32,9 +31,67 @@ animation1_free(AnimationContext * const ctx)
 static void
 animation1_reset_state(AnimationContext* const ctx)
 {
-    ctx->elapsed = 0.f;
     ctx->current_size = 0.f;
-    ctx->resize_time = 2.f;
+    ctx->current_angle = 0.f;
+}
+
+static void expand_square(AnimationContext * const ctx, float const resize_time)
+{
+    float elapsed = 0;
+
+    while (elapsed < resize_time)
+    {
+        float const f = elapsed / resize_time;
+
+        ctx->current_size = Lerp(0.f, ctx->max_size, ease_out_cubic(f));
+        elapsed += ctx->env->delta;
+        coroutine_yield(ctx->schedule);
+    }
+    ctx->current_size = ctx->max_size;
+}
+
+static void shrink_square(AnimationContext * const ctx, float const resize_time)
+{
+    float elapsed = 0;
+
+    while (elapsed < resize_time)
+    {
+        float const f = elapsed / resize_time;
+
+        ctx->current_size = Lerp(ctx->max_size, 0, ease_out_cubic(f));
+        elapsed += ctx->env->delta;
+        coroutine_yield(ctx->schedule);
+    }
+    ctx->current_size = 0;
+}
+
+static void
+rotate_square(AnimationContext * const ctx, float const angle_degrees, float const rotate_time)
+{
+    float elapsed = 0;
+    float current_angle = 0;
+    float start_angle = ctx->current_angle;
+
+    while (elapsed < rotate_time)
+    {
+        float const f = elapsed / rotate_time;
+
+        ctx->current_angle = start_angle + Lerp(0.f, angle_degrees, ease_out_cubic(f));
+        elapsed += ctx->env->delta;
+        coroutine_yield(ctx->schedule);
+    }
+    ctx->current_angle = start_angle + angle_degrees;
+}
+
+static void animation_sleep(AnimationContext * const ctx, float const sleep_time)
+{
+    float elapsed = 0;
+
+    while (elapsed < sleep_time)
+    {
+        elapsed += ctx->env->delta;
+        coroutine_yield(ctx->schedule);
+    }
 }
 
 static void
@@ -44,15 +101,22 @@ animation_coroutine(struct schedule * const s, void * const arg)
 
     animation1_reset_state(ctx);
 
-    while (ctx->elapsed < ctx->resize_time)
-    {
-        float const f = ctx->elapsed / ctx->resize_time;
+    expand_square(ctx, 0.25f);
+    animation_sleep(ctx, 0.25f);
+    rotate_square(ctx, 45.f, 0.25f);
+    animation_sleep(ctx, 0.25f);
+    rotate_square(ctx, -45.f, 0.25f);
+    animation_sleep(ctx, 0.25f);
+    shrink_square(ctx, 0.25f);
+}
 
-        ctx->current_size = Lerp(0.f, ctx->max_size, ease_out_cubic(f));
-        ctx->elapsed += ctx->env->delta;
-        coroutine_yield(s);
+static void
+update_animations(AnimationContext * const ctx)
+{
+    if (coroutine_active_count(ctx->schedule) > 0)
+    {
+        coroutine_resume(ctx->schedule);
     }
-    ctx->current_size = ctx->max_size;
 }
 
 void
@@ -87,10 +151,7 @@ animation1_update(AnimationContext * const ctx, Environment const * const env)
     assert(ctx != NULL);
     ctx->env = env;
 
-    if (coroutine_active_count(ctx->schedule) > 0)
-    {
-        coroutine_resume(ctx->schedule);
-    }
+    update_animations(ctx);
 
     int const screen_width = GetScreenWidth();
     int const screen_height = GetScreenHeight();
@@ -98,9 +159,14 @@ animation1_update(AnimationContext * const ctx, Environment const * const env)
     int const screen_center_y = screen_height / 2;
 
     int const square_size = ctx->current_size;
-    int const pos_x = screen_center_x - (square_size / 2);
-    int const pos_y = screen_center_y - (square_size / 2);
+    int const pos_x = screen_center_x;
+    int const pos_y = screen_center_y;
 
-    DrawRectangle(pos_x, pos_y, square_size, square_size, RED);
+    DrawRectanglePro(
+        (Rectangle){ pos_x, pos_y, square_size, square_size },
+        (Vector2) { square_size / 2.f, square_size / 2.f },
+        ctx->current_angle,
+        RED
+    );
 }
 
